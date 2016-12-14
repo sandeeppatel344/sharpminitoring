a9b995d477f4874cde9caa09c2b4bddc1e11cdc9
sandeeppatel344 <sandeeppatel344@gmail.com>
2016-12-14 19:32:07 +0530

    new changes

---
+++ WebContent/app/entriesform/config/entriesconfig.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sandeep on 12/6/2016.
+ */
+app.config([ '$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+        //$urlRouterProvider.otherwise('sharpmonitoring/channel');
+        $stateProvider.state('sharpmonitoring.entriesform', {
+            url : '/entriesform',
+            views : {
+                '@' : {
+                    templateUrl : 'app/entriesform/views/entriesform.html',
+                    controller:'entriesformCtrl'
+                }
+            }
+
+        })
+    }]);
+
---
+++ WebContent/app/entriesform/services/entriesformservice.js
@@ -0,0 +1,20 @@
+app.factory("entriesformService",function($http){
+	var obj = {};
+	obj.saveEntries = function(postdata){
+		return $http.post(userserviceapiurl+"entries/add",postdata)
+	}
+	obj.getAllEntries = function(){
+		return $http.post(userserviceapiurl+"entries/add",postdata)
+	}
+	obj.editEntries = function(id){
+		return $http.post(userserviceapiurl+"entries/edit?id=",id)
+	}
+	obj.updateEntries = function(postdata){
+		return $http.post(userserviceapiurl+"entries/update",postdata)
+	}
+	obj.deleteEntries = function(id){
+		return $http.post(userserviceapiurl+"entries/delete?id=",id)
+	}
+	
+	return obj;
+})
